e6a2fdcb9b8af3ad0049485680f20561778011d7
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-21 20:55:19 +0300

    lab2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+///lab1
+
 //1
 let lengthRectangle=5;
 let WidthRectangle=4;
@@ -41,7 +43,90 @@ let hobby='play video games'
 let awesomeMessage="Hi, my name is "+ firstName + " I love "+interest +" In my spare time, I like to "+ hobby
 console.log(awesomeMessage)
 
+//lab2
+//1
+var eatsPlants = true;
+var eatsAnimals = false;
+var result = eatsPlants ?  (eatsAnimals ? "omnivore" : "herbivore") : (eatsAnimals ? "carnivore" : undefined);
+    console.log(eatsPlants ,eatsAnimals,result );
+//2
 
+var musicians = 5;
+if(musicians <= 0){
+console.log("not a group")
+}
+else if (musicians == 1){
+    console.log("solo") 
+}
+else if (musicians == 2){
+    console.log("duet") 
+}
+else if (musicians == 3){
+    console.log("trio") 
+}
+else if (musicians == 4){
+    console.log("quartet") 
+}
+else  console.log("this is a large group") 
 
+//3
+var room = "gallery room";
+var suspect = "Ms. Van Cleve";
 
+var weapon = "";
+var solved = false;
 
+if (room === "ballroom room" && suspect === "Mr. Kalehoff") {
+    weapon = "poison"; 
+    solved = true;
+  
+} else if (room === "gallery room" && suspect === "Ms. Van Cleve") {
+    weapon = "trophy";
+     solved = true;
+  
+
+} else if (room === "billiards room" && suspect === "Mrs. Sparr") {
+    weapon = "pool stick"; 
+    solved = true;
+
+} else {
+    weapon = "knife"; 
+    solved = true;
+    suspect = "Mr. Parkes";
+    room = "dining room"
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
+
+//4
+var balance = -1;
+var checkBalance = true;
+var isActive = true;
+
+if (!checkBalance) {
+console.log("Thank you.Have a nice day!");
+} else if(isActive === true && balance > 0) {
+console.log("Your balance is $" + balance.toFixed(2) + ".");
+} else if (!isActive) {
+console.log("Your account is no longer active.");
+} else if (isActive && balance === 0) {
+console.log("Your account is empty.");
+} else if (isActive && balance < 0) {
+console.log("Your balance is negative. Please contact bank.");
+}
+
+//5
+var flavor = "chocolate";
+var vessel = "cone";
+var toppings = "sprinkles";
+if(flavor === "vanilla" || flavor === "chocolate") {
+if(flavor === "vanilla" && vessel === "cone" || vessel === "bowl" && toppings === "spirnkles" || toppings === "peanuts"){
+console.log( "I'd like two scoops of "+ flavor + " ice cream in a "+ vessel + " with " + toppings +".")
+} else if (flavor === "chocolate" && vessel === "cone" || vessel === "bowl" && toppings === "spirnkles" || toppings === "peanuts"){
+console.log( "I'd like two scoops of "+ flavor + " ice cream in a "+ vessel + " with " + toppings +".")
+} else {
+console.log("pick vanilla or chocolate ");
+}
+}
