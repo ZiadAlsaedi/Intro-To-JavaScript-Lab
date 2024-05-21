ec82ecdbcc8f9ed09d7803e51f6f4bb996a63361
ZiadAlsaedi <ziad12399@gmail.com>
2024-05-21 18:40:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+//1
+let lengthRectangle=5;
+let WidthRectangle=4;
+let Rectangle=lengthRectangle*WidthRectangle;
+console.log(Rectangle)
+//2
+let temperatureC =50;
+let tempToF= (temperatureC*1.8)+32;
+console.log(temperatureC +" C is "+ tempToF+" F");
+
+let temperatureF =122;
+let tempToC=(temperatureF-32)/1.8;
+console.log(temperatureF +" F is "+ tempToC+" C");
+
+//3
+let hours=3;
+let second=10800;
+let result3= hours*3600;
+console.log(hours +" hours  = "+ result3 +" second");
+//4
+let thingOne = "Ziad";
+let thingTwo = " Alsaedi";
+console.log(thingOne + thingTwo);
+//5
+var bill=10.25 + 3.99 + 7.15;
+var tip=bill* 0.15;
+var total =bill+tip;
+console.log("Total is " + total)
+
+//6
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining"
+let madLib ="The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+console.log(madLib);
+
+//7 
+let firstName='ziad'
+let interest='footbal'
+let hobby='play video games'
+let awesomeMessage="Hi, my name is "+ firstName + " I love "+interest +" In my spare time, I like to "+ hobby
+console.log(awesomeMessage)
+
+
+
+
+
